4d64cd055cb95e6e0bf402120573b370e6be6dec
Emnolope <emmanuelnlopez+15@gmail.com>
2024-04-07 16:18:46 +0000

    done todo list skeleton

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,13 @@
+<div>
+  <form action="" method="">
+    <input type="hidden" name="done" value="">
+    <button aria-label="Toggle done">Toggle done</button>
+  </form>
+  <form action="" method="">
+    <input type="text" name="" id="">
+    <button aria-label="Save">Save</button>
+  </form>
+  <form action="" method="">
+    <button aria-label="Delete">Delete</button>
+  </form>
+</div>
